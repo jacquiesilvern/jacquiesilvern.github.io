7c4145faa3705cdbfdb164cdca8d481159e2b977
jacquiesilvern <jasi7931@colorado.edu>
2022-10-30 23:37:09 -0600

    assignment 10 final

--- assignments/assignment 10/assignment10.js
+++ assignments/assignment 10/assignment10.js
@@ -2,36 +2,87 @@
 const Employees = {
     
 let Sam : {
-        "First Name": "Sam",
+        "FirstName": "Sam",
         "Department": "Tech",
         "Salary": 40000,
-        "Raise Eligible": "True"
-      },
+        "RaiseEligible": "True"
+      };
+
 let Mary : {
-        "First Name": "Mary",
+        "FirstName": "Mary",
         "Department": "Finance",
         "Salary": 18500,
-        "Raise Eligible": "True"
-      },
+        "RaiseEligible": "True"
+      };
+
 let Bill : {
-        "First Name": "Bill",
+        "FirstName": "Bill",
         "Department": "HR",
         "Salary": 21200,
-        "Raise Eligible": "False"
-      }
+        "RaiseEligible": "False"
+      };
 
+console.log("Problem 1:", Sam, Mary, Bill)
+ /* Problem 3 */     
 let Anna : {
+  "FirstName": "Anna",
+  "Department": "Tech",
+  "Salary": 25600,
+  "RaiseEligible": "False"
 
 }
   }
 
+  console.log("Problem 3:", Sam, Mary, Bill, Anna)
+
 /* Problem 2 */
 
-  const Company = {
+const Company = {
 let, tCompany =  {
         "companyName": " Tech Stars",
         "website": " www.techstars.site",
-        "Employees": [Sam, Mary, Bill]
+        "Employees": [Sam, Mary, Bill, Anna]
     }
 
   }
+
+  console.log("Problem 2:", tCompany)
+
+/* Problem 4 */
+
+  let total = 0;
+  for (let i = 0; i < tCompany.Employees.length; i++){
+    totalSalary += tCompany.Employees[i].Salary
+  }
+
+  console.log(totalSalary)
+
+/* Problem 5 */
+
+for (let i = 0; i < tCompany.Employees.length; i++){
+  if(tCompany.Employees[i].RaiseEligible === "True"){
+    let pSalary = tCompany.Employees[i].Salary;
+    tCompany.Employees[i].Salary = tCompany.Employees[i].Salary * 1.1;
+    tCompany.Employees[i].RaiseEligible = "False"
+  }
+}
+
+console.log("Problem 5", company)
+
+/* Problem 6 */
+
+let wfh = ["Anna", "Sam"]
+
+for (let i = 0; i <tCompany.Employees.length; i++){
+  if (wfh.includes(Sam.FirstName)){
+    Sam.wfh = "true"
+  }
+  else if (wfh.includes(Anna.FirstName)){
+    Anna.wfh = "true"
+  }
+  else {
+    wfh = "false"
+  }
+}
+
+
