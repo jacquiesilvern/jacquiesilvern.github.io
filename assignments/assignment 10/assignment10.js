d6111c9051a86d884c6adfbc2d66d5ccd3d5584d
jacquiesilvern <jasi7931@colorado.edu>
2022-10-30 22:41:27 -0600

    assignment10

---
+++ assignments/assignment 10/assignment10.js
@@ -0,0 +1,37 @@
+/* Problem #1: */
+const Employees = {
+    
+let Sam : {
+        "First Name": "Sam",
+        "Department": "Tech",
+        "Salary": 40000,
+        "Raise Eligible": "True"
+      },
+let Mary : {
+        "First Name": "Mary",
+        "Department": "Finance",
+        "Salary": 18500,
+        "Raise Eligible": "True"
+      },
+let Bill : {
+        "First Name": "Bill",
+        "Department": "HR",
+        "Salary": 21200,
+        "Raise Eligible": "False"
+      }
+
+let Anna : {
+
+}
+  }
+
+/* Problem 2 */
+
+  const Company = {
+let, tCompany =  {
+        "companyName": " Tech Stars",
+        "website": " www.techstars.site",
+        "Employees": [Sam, Mary, Bill]
+    }
+
+  }
