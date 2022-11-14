f9d04e810621d7462f2326c0090e935895d6bb4f
jacquiesilvern <jasi7931@colorado.edu>
2022-11-13 17:55:22 -0700

    wa12

--- assignments/wa12/wa12.js
+++ assignments/wa12/wa12.js
@@ -1,30 +1,9 @@
 
-function check () {
-    console.log('test');
-}
-
 function submit() {
-    alert (output.textContent);
+    alert ('This is not your number. Please Restart!', output.textContent);
+    output.textContent = 000000000; 
 }
 
-function reset() {
-    outputInt = 0;
-    output.textContent = outputInt;
-}
-
-function minus () {
-if (outputInt > 0) {
-    outputInt -=1;
-    output.textContent = outputInt;
-}
-
-}
-function plus() {
-    if (outputInt < 9999999999) {
-    outputInt += 1;
-    output.textContent = outputInt;
-}
-}
 function random() {
     outputInt = randomNumber (0, 9999999999);
     output.textContent = outputInt;
@@ -39,21 +18,7 @@ function randomNumber(min, max) {
 const output = document.querySelector('.output');
 let outputInt = parseInt (output.textContent);
 
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
+
 const randomButton = document.querySelector('.random-button').addEventListener('click', random);
-const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
 const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
 
-/*const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-
-button.addEventListener('click', updateOutput);
-
-function updateOutput() {
-    output.textContent = phone_content.value;
-    // how to make something pop up on the screen alert(phone_content.value)
-}
-
-*/
